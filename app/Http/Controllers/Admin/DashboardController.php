73582071e1613655b71c3080f3f0bff3fca9ea2f
Pavel Kurliuk <freel.k@gmail.com>
2018-09-05 13:55:12 +0500

    add admin dashboard

--- app/Http/Controllers/Admin/DashboardController.php
+++ app/Http/Controllers/Admin/DashboardController.php
@@ -7,5 +7,8 @@ use App\Http\Controllers\Controller;
 
 class DashboardController extends Controller
 {
-    //
+    //DashboardController@index
+    public function index(){
+      return view('admin.index');
+    }
 }
