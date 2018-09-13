36796036a7b91259046f8f2b890ee17bd5e821e1
Pavel Kurliuk <freel.k@gmail.com>
2018-09-13 17:50:26 +0500

    change

--- resources/views/test/index.blade.php
+++ resources/views/test/index.blade.php
@@ -7,16 +7,18 @@
 
         @if ($group->papers->count())
           <div class="col-lg-8">
-            <span>{{ $group->description_short }}</span>
+            <span>{{ $group->title }}</span>
             <a href="{{ route('test.exam', $group ) }}" class="btn btn-primary">Начать тест</a>
           </div>
         @else
           <div class="col-lg-8">
-            <a href="{{ route('test.index', $group ) }}">{{ $group->description_short }}</a>
+            <a href="{{ route('test.index', $group ) }}">{{ $group->title }}</a>
           </div>
         @endif
       </div>
     @endforeach
+
+    {{ $test_groups->links() }}
   </div>
   {{-- @isset($start_test)
     <div class="container">
